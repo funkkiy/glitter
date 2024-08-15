a0cef3219b252a29b2f082af8262a1b2587ec3f4
Davi <davicr38@gmail.com>
2024-08-14 21:56:06 -0300

    Fix `m_elementCount` type

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,24 @@
 #include <cstdlib>
 #include <ctime>
 
+template <typename Into, typename From> constexpr Into narrow_into(From x)
+{
+    static_assert(std::is_arithmetic_v<From> && std::is_arithmetic_v<Into>, "narrow_into requires arithmetic types");
+    static_assert(std::is_integral_v<From> && std::is_integral_v<Into>, "narrow_into requires integral types");
+    static_assert(!std::is_same_v<Into, From>, "narrow_into requires From and Into to be different types");
+    static_assert(sizeof(From) >= sizeof(Into), "narrow_into requires From to be bigger than Into");
+
+    if (std::cmp_less(x, std::numeric_limits<Into>::min())) {
+        return std::numeric_limits<Into>::min();
+    }
+
+    if (std::cmp_greater(x, std::numeric_limits<Into>::max())) {
+        return std::numeric_limits<Into>::max();
+    }
+
+    return static_cast<Into>(x);
+}
+
 class LinearAllocator {
 public:
     LinearAllocator() {};
@@ -72,7 +90,7 @@ struct Primitive {
     GLuint m_vbo;
     GLuint m_ebo;
     GLuint m_baseTexture;
-    uint64_t m_elementCount;
+    GLsizei m_elementCount;
 };
 
 struct Mesh {
@@ -368,7 +386,7 @@ private:
                 cgltf_free(data);
 
                 for (auto& primitives : parsedMeshes[0].m_primitives) {
-                    Primitive primitive {.m_elementCount = primitives.m_vertexIndices.size()};
+                    Primitive primitive {.m_elementCount = narrow_into<GLsizei>(primitives.m_vertexIndices.size())};
 
                     // Create VBO.
                     GLuint VBO;
