9762ea828480926ba03e46aefeaa9c0908d2cd38
Davi <davicr38@gmail.com>
2024-08-31 09:58:55 -0300

    Add precompiled header

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,7 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/random.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include <cgltf.h>
-#include <spdlog/spdlog.h>
 #include <stb_image.h>
 
 #include <imgui.h>
