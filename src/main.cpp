100289566b0c950c832157a608f3536b8dbc2ae0
Davi <davicr38@gmail.com>
2024-12-10 21:11:50 -0300

    Split off `narrow_into` into new file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "glitter/Config.h"
 #include "glitter/ImGuiConfig.h"
 #include "glitter/util/File.h"
+#include "glitter/util/Common.h"
 #include "glitter/gfx/LinearAllocator.h"
 
 #define GLFW_INCLUDE_NONE
@@ -25,24 +26,6 @@
 #include <cstdint>
 #include <cstdlib>
 #include <ctime>
-
-template <typename Into, typename From> constexpr Into narrow_into(From x)
-{
-    static_assert(std::is_arithmetic_v<From> && std::is_arithmetic_v<Into>, "narrow_into requires arithmetic types");
-    static_assert(std::is_integral_v<From> && std::is_integral_v<Into>, "narrow_into requires integral types");
-    static_assert(!std::is_same_v<Into, From>, "narrow_into requires From and Into to be different types");
-    static_assert(sizeof(From) >= sizeof(Into), "narrow_into requires From to be bigger than Into");
-
-    if (std::cmp_less(x, std::numeric_limits<Into>::min())) {
-        return std::numeric_limits<Into>::min();
-    }
-
-    if (std::cmp_greater(x, std::numeric_limits<Into>::max())) {
-        return std::numeric_limits<Into>::max();
-    }
-
-    return static_cast<Into>(x);
-}
 
 namespace Glitter {
 
