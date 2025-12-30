2eebd8421ad86f62604faafcdf60fb9043689003
Davi <davicr38@gmail.com>
2025-12-30 11:25:38 -0300

    Fix `CreateVAO` iteration variable type

--- src/glitter/gfx/VAO.cpp
+++ src/glitter/gfx/VAO.cpp
@@ -8,7 +8,7 @@ GLuint CreateVAO(std::string_view name, std::initializer_list<VAOAttribute> attr
     glCreateVertexArrays(1, &vao);
     glObjectLabel(GL_VERTEX_ARRAY, vao, -1, name.data());
 
-    for (int i = 0; i < attributes.size(); i++) {
+    for (GLuint i = 0; i < attributes.size(); i++) {
         const VAOAttribute& attrib = *(attributes.begin() + i);
         glEnableVertexArrayAttrib(vao, i);
         glVertexArrayAttribFormat(vao, i, attrib.m_size, attrib.m_type, attrib.m_normalized, attrib.m_offset);
